5f5c023aa5110987aae23ced91c96aa6498c482c
taki-deen <57011692+taki-deen@users.noreply.github.com>
2025-05-03 16:35:12 +0300

    .

--- src/config/seed.js
+++ src/config/seed.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const { Author, Book, Bookshop } = require('./models');
+const { Author, Book, Bookshop } = require('../models');
 
 // Sample data
 const authors = [
